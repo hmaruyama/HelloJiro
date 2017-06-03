0b479d9301a0ea7089f2da6cdd5de77046cc5f96
hmaruyama <maruyama@val.co.jp>
2017-06-03 12:05:53 +0900

    サンプルワードから二郎の呪文に変更

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ julius.each_message do |message, prompt|
   when :RECOGOUT
     voice_message = message.sentence
     puts voice_message
-    if voice_message.include?("天気") || voice_message.include?("にんにく")
+    if voice_message.include?("ニンニク")
       puts "にんにく♪───Ｏ（≧∇≦）Ｏ────♪"
     end
     break
