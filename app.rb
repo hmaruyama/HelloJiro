cb1ae576c6bab97bfa8522b605439d5a6d913e41
hmaruyama <maruyama@val.co.jp>
2017-06-03 11:56:14 +0900

    シンプルな記述に変更

--- app.rb
+++ app.rb
@@ -1,36 +1,15 @@
-# main.rb
-SRC_DIR = File.expand_path(File.dirname(__FILE__))
-
-require 'rubygems'
 require 'julius'
 
-class HeyRaspi
-  def run(julius)
-    begin
-      julius.each_message do |message, prompt|
-        case message.name
-        when :RECOGOUT
-          prompt.pause
-
-          shypo = message.first
-          whypo = shypo.first
-          confidence = whypo.cm.to_f
-
-          puts "#{message.sentence} #{confidence}"
-
-          prompt.resume
-        end
-      end
-    rescue REXML::ParseException
-      puts "retry…"
-      retry
+voice_message = ''
+julius = Julius.new
+julius.each_message do |message, prompt|
+  case message.name
+  when :RECOGOUT
+    voice_message = message.sentence
+    puts voice_message
+    if voice_message.include?("天気") || voice_message.include?("にんにく")
+      puts "にんにく♪───Ｏ（≧∇≦）Ｏ────♪"
     end
+    break
   end
 end
-
-puts "接続中…"
-julius = Julius.new
-
-puts "準備OK！"
-hey_raspi = HeyRaspi.new
-hey_raspi.run(julius)
